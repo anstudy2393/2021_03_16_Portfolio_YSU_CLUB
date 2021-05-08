fc05a1bfe6ab4ca89409d868e4e0906d351da787
LEESANGNAM <61412496+LEESANGNAM@users.noreply.github.com>
2021-05-08 13:33:11 +0900

    동아리페이징 (#35)

--- src/main/java/kr/ac/yeonsung/demo/repository/ClubRepository.java
+++ src/main/java/kr/ac/yeonsung/demo/repository/ClubRepository.java
@@ -4,6 +4,7 @@ import kr.ac.yeonsung.demo.domain.Join;
 import kr.ac.yeonsung.demo.domain.JoinClub;
 import kr.ac.yeonsung.demo.domain.club.Club;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,43 +13,6 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class ClubRepository {
-    @PersistenceContext
-    private final EntityManager em;
-
-
-    /**
-     * 생성
-     */
-    public void save(Club club){
-        if (club.getId() ==null){
-            em.persist(club);
-        }else {
-            em.merge(club);
-        }
-    }
-    /**
-     * 하나조회
-     */
-    public Club findOne(Long id){
-        return em.find(Club.class,id);
-    }
-    /**
-     * 모두조회
-     */
-    public List<Club> findAll(){
-        return em.createQuery("select c from Club c",Club.class).getResultList();
-    }
-
-    /**
-     * 삭제
-     */
-
-    public void deleteOne(Club club){
-        em.remove(club);
-    }
-
-
+public interface ClubRepository extends JpaRepository<Club,Long> {
 
 }
