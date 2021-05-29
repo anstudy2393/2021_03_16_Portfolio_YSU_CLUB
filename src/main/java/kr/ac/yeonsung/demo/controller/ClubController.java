35f95fac8766a39b0390e1d121f90ddcea18d8e7
LEESANGNAM <61412496+LEESANGNAM@users.noreply.github.com>
2021-05-29 13:03:50 +0900

    13주(vaild완료,테스트코드완료) (#54)
    * 13주(vaild완료,테스트코드완료)
    
    * 13주(vaild완료,테스트코드완료)

--- src/main/java/kr/ac/yeonsung/demo/controller/ClubController.java
+++ src/main/java/kr/ac/yeonsung/demo/controller/ClubController.java
@@ -9,44 +9,69 @@ import kr.ac.yeonsung.demo.service.ClubService;
 import kr.ac.yeonsung.demo.service.JoinClubService;
 import kr.ac.yeonsung.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Null;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ClubController {
     private final ClubService clubService;
     private final JoinClubService joinClubService;
     private final MemberService memberService;
+    private Object Null;
 
 
     @GetMapping("/clubs/new")
     public String createForm(Model model) {
         List<Member> members = memberService.findMembers();
-
-        model.addAttribute("form", new BookForm());
+        try {
+            members.get(0).getId();
+        }catch (Exception e) {
+            return "clubs/clubListError";
+        }
+        model.addAttribute("bookForm", new BookForm());
         model.addAttribute("members",members);
         return "clubs/createClubForm";
     }
 
     @PostMapping("/clubs/new")
-    public String create(BookForm form, @RequestParam("memberId") Long memberId) {
+    public String create(@Valid BookForm bookForm,BindingResult result, @RequestParam("memberId") Long memberId,Model model) {//BindingResult form에서 오류가 있을시 오류가 result에 담김
+        if(result.hasErrors()){
+            List<Member> members = memberService.findMembers();
+            model.addAttribute("members",members);
+            return "clubs/createClubForm";
+        }
         Book book = new Book();
         Member member = memberService.findOne(memberId);
-        book.setName(form.getName());
-        book.setTotalNumber(form.getTotalNumber());
+        book.setName(bookForm.getName());
+        book.setTotalNumber(bookForm.getTotalNumber());
         book.setClubJang(member.getName());
+        try {
+            clubService.saveClub(book);
+        }catch (IllegalStateException e){
+            String message = e.getMessage();
+            log.info(message);
+            model.addAttribute("message",message);
+            List<Member> members = memberService.findMembers();
+            model.addAttribute("members",members);
 
-        clubService.saveClub(book);
+            return "clubs/createClubForm";
+        }
+
 
         return "redirect:/clubs";
     }
@@ -75,16 +100,32 @@ public class ClubController {
         List<Member> members = memberService.findMembers();
 
         Club club = clubService.findOne(clubId);
-        model.addAttribute("form",club);
+        model.addAttribute("bookForm",club);
         model.addAttribute("members",members);
         return "clubs/clubInfoChange";
     }
 
     @PostMapping("/clubs/{clubId}/change")
-    public String change(BookForm form,@PathVariable("clubId") Long clubId,@RequestParam("memberId") Long memberId){
-        Member member = memberService.findOne(memberId);
-        clubService.updateClub(clubId,form.getName(),form.getTotalNumber(),
-                member.getName());
+    public String change(@Valid BookForm bookForm, BindingResult result,@PathVariable("clubId") Long clubId,@RequestParam("memberId") Long memberId,Model model){
+        if(result.hasErrors()){
+            List<Member> members = memberService.findMembers();
+            model.addAttribute("members",members);
+            return "clubs/clubInfoChange";
+        }
+
+       try {
+           Member member = memberService.findOne(memberId);
+           clubService.updateClub(clubId, bookForm.getName(), bookForm.getTotalNumber(),
+                   member.getName());
+       }catch (IllegalStateException e){
+           String message = e.getMessage();
+           log.info(message);
+           model.addAttribute("message",message);
+           List<Member> members = memberService.findMembers();
+           model.addAttribute("members",members);
+
+           return "clubs/clubInfoChange";
+       }
         return "redirect:/clubs";
     }
 
--- src/main/java/kr/ac/yeonsung/demo/repository/ClubRepository.java
+++ src/main/java/kr/ac/yeonsung/demo/repository/ClubRepository.java
@@ -2,8 +2,10 @@ package kr.ac.yeonsung.demo.repository;
 
 import kr.ac.yeonsung.demo.domain.Join;
 import kr.ac.yeonsung.demo.domain.JoinClub;
+import kr.ac.yeonsung.demo.domain.Member;
 import kr.ac.yeonsung.demo.domain.club.Club;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +16,5 @@ import java.util.List;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club,Long> {
-
+    List<Club> findByName(String name);
 }
